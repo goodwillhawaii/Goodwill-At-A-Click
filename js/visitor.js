dc738bc6e3455e479374c90f548ce9f91d4d8c88
goodwillhawaii <48804587+goodwillhawaii@users.noreply.github.com>
2019-10-30 14:24:41 -1000

    tentative final version

---
+++ js/visitor.js
@@ -0,0 +1,12 @@
+function dailyVisitLog() {
+    var visitorFname;
+
+    var firstName = document.getElementById("fname").value;
+    var lastName =  document.getElementById("lname").value;
+    var date = document.getElementById("dayvisited").value;
+    var timeIn = document.getElementById("timein").value;
+    var destinationSponsor = document.getElementById("dayvisited").value;
+    var timeOut = document.getElementById("timeout").value;
+
+    document.getElementById("fname").innerHTML = visitorFname;
+}
